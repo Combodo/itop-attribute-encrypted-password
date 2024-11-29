ee71e747adc7226ba18db3ec981ab3f183a6e351
odain <olivier.dain@combodo.com>
2024-11-29 10:58:30 +0100

    composer regenerated

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,11 +23,20 @@ class ComposerAutoloaderInit95a1aeae22f28212f64d406babfc3627
         }
 
         spl_autoload_register(array('ComposerAutoloaderInit95a1aeae22f28212f64d406babfc3627', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit95a1aeae22f28212f64d406babfc3627', 'loadClassLoader'));
 
-        require __DIR__ . '/autoload_static.php';
-        call_user_func(\Composer\Autoload\ComposerStaticInit95a1aeae22f28212f64d406babfc3627::getInitializer($loader));
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        if ($useStaticLoader) {
+            require __DIR__ . '/autoload_static.php';
+
+            call_user_func(\Composer\Autoload\ComposerStaticInit95a1aeae22f28212f64d406babfc3627::getInitializer($loader));
+        } else {
+            $classMap = require __DIR__ . '/autoload_classmap.php';
+            if ($classMap) {
+                $loader->addClassMap($classMap);
+            }
+        }
 
         $loader->setClassMapAuthoritative(true);
         $loader->register(true);
